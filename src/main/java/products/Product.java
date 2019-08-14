69061c15400d2c8832ad3b0e8b8f26e9d9db2f8d
Anton Azarnin <AAzarnin@luxoft.com>
2019-08-14 16:41:44 +0300

    overriding equals() and hashCode()

--- src/main/java/products/Product.java
+++ src/main/java/products/Product.java
@@ -10,6 +10,16 @@ public class Product {
     }
 
     @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
     public String toString() {
         return "Product{" +
                 "code='" + code + '\'' +
