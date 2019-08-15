1a56e2658832c9ab7505c935e00ded6d3463fd42
Anton Azarnin <AAzarnin@luxoft.com>
2019-08-15 11:58:39 +0300

    adding SortArray 2

--- src/main/java/SortArray.java
+++ src/main/java/SortArray.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,15 +18,26 @@ public class SortArray {
     Integer[] a3 = {1,2,3,2};
 
     @Test
-    public void sortArrayUnique(){
+    public void sortUniqueValuesViaStream(){
         Stream<Integer> streamFromA1 = Arrays.stream(a1);
         System.out.println("Unique values in a1 array are: "+ streamFromA1.distinct().collect(Collectors.toList()));
         Stream<Integer> streamFromA2 = Arrays.stream(a2);
         System.out.println("Unique values in a2 array are: "+ streamFromA2.distinct().collect(Collectors.toList()));
         Stream<Integer> streamFromA3 = Arrays.stream(a3);
         System.out.println("Unique values in a3 array are: "+ streamFromA3.distinct().collect(Collectors.toList()));
+    }
+
+    @Test
+    public void sortUniqueValuesViaSet(){
+        Set<Integer> setFromA1 = new HashSet<>();
+        setFromA1.addAll(a1);
+        System.out.println("Unique values in a1 array are: "+ setFromA1.toString());
 
     }
+
+    /* Есть массив А. Надо переложить его в массив В,
+    оставив там только НЕПОВТОРЯЮЩИЕСЯ УНИКАЛЬНЫЕ значения.*/
+
 
 
 
@@ -39,4 +50,7 @@ public class SortArray {
      2) {1,2,3,4}        | {}
      3) {1,2,3,2}        | {2,2}
      */
+
+
 }
+
